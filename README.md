c7af10a665d7bfc76301e76ac3bc0be3ed326f0f
makikovaughan <makiko.vaughan@gmail.com>
2023-08-10 01:06:38 -0400

    Created ConsoleCreatures.cs and Readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # C_Sharp_Study
+
+## These files were all projects for Codecademy C# course
+
+1. ConsoleCreatures.cs: Drew a ghost and dancing man usnig Console.Writeln().
---
+++ app/ConsoleCreatures.cs
@@ -0,0 +1,40 @@
+using System;
+
+/*
+Console Creatures
+Using the Console.WriteLine() command to draw a ghost.
+*/
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine(" .-.");    
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |");
+      Console.WriteLine("'~~~'");
+      Console.WriteLine("Makiko's Ghost");
+      Console.WriteLine("\n");
+      
+      //Draw a dancing man
+      Console.WriteLine("c_     ___");
+      Console.WriteLine("　 \\\\__|__|__");
+      Console.WriteLine("　　 \\( o_o)");
+      Console.WriteLine("　　　 > ~  >");
+      Console.WriteLine("　　　/ 　 / \\\\");
+      Console.WriteLine("　　 /　　/　 \\\\");
+      Console.WriteLine("　　 \\　 )　　c/");
+      Console.WriteLine("　　/　 /");
+      Console.WriteLine("　 /　/|");
+      Console.WriteLine("　(　( \\");
+      Console.WriteLine("　|　|  \\");
+      Console.WriteLine("　| / \\  )");
+      Console.WriteLine("　| |　) |");
+      Console.WriteLine(" /  )(_/");
+      Console.WriteLine("(_ /");
+      Console.WriteLine("-Dancing Man");
+     }
+  }
+}
